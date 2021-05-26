a40ef36cd160f342096118d206559f82ef16b178
Danilo Castro <danilogcastro@hotmail.com>
2021-05-26 14:01:15 -0300

    Populated views for Restaurant and created actions in Review controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,27 @@
 class ReviewsController < ApplicationController
+  before_action :find_restaurant
+
   def new
+    @review = Review.new
   end
 
   def create
+    @review = Review.find(review_params)
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def find_restaurant
+    @restaurant = Restaurand.find(params[:restaurant_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content)
   end
 end
